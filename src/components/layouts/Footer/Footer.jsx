79b70ff13bc54ee8dc21971c0c13008eaafcd946
MurotjonAbidjanov <murotzonabidzanov@gmail.com>
2024-05-23 11:09:19 +0600

    full

--- src/components/layouts/Footer/Footer.jsx
+++ src/components/layouts/Footer/Footer.jsx
@@ -1,132 +1,109 @@
 import { Link } from 'react-router-dom'
 import LogoIcon from '../Navbar/images/LogoIcon'
-import { FaCcApplePay, FaCcVisa, FaCcDiscover, FaCcMastercard } from 'react-icons/fa'
+import {
+  FaCcApplePay,
+  FaCcVisa,
+  FaCcDiscover,
+  FaCcMastercard,
+  FaPhoneSquareAlt,
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+} from 'react-icons/fa'
 import { RiSecurePaymentFill } from 'react-icons/ri'
+import { CgMail } from 'react-icons/cg'
 
 export default function Footer() {
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-    })
+    window.scrollTo({ top: 0 })
   }
 
   return (
-    <div className="w-full bg-gray-900">
-      <div className="max-w-[1320px] m-auto ">
-        <div className="flex items-center justify-between py-[60px] flex-wrap">
-          <div className="flex flex-col gap-4 max-w-[336px]">
+    <div className="w-full bg-gray-800 text-gray-300 text-xs">
+      <div className="max-w-[1320px] mx-auto py-5 px-4">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-5">
             <Link onClick={scrollToTop} to="/">
-              <LogoIcon color="white" />
+              <LogoIcon color="white" className="w-24 h-24" />
             </Link>
-            <Link onClick={scrollToTop} to="/contact">
-              <div className="flex gap-1">
-                <div className="text-white border-b-2 pb-[6px] border-primary">(996) 555 55-55-55</div>
-                <p className="text-gray-500 text-[14px]">or</p>
-                <div className="text-white border-b-2 pb-[6px] border-primary">
-                  mega@gmail.com
+            <div className="flex flex-col gap-3">
+              <Link onClick={scrollToTop} to="/contact">
+                <div className="flex items-center gap-2 text-white pt-[15px] p-2 border-b border-primary transition duration-300 hover:text-primary">
+                  <FaPhoneSquareAlt className="text-lg" /> (+996) 555 55-55-55
                 </div>
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[16px] text-white">My Account</h1>
-            <div className="flex flex-col gap-3 text-gray-400 ">
-              <Link onClick={scrollToTop} to="/" className="hover:text-white text-[14px]">
-                My Account
-              </Link>
-              <Link onClick={scrollToTop} to="/" className="hover:text-white text-[14px]">
-                Order History
-              </Link>
-              <Link onClick={scrollToTop} to="/" className="hover:text-white text-[14px]">
-                Shoping Cart
-              </Link>
-              <Link onClick={scrollToTop} to="/" className="hover:text-white text-[14px]">
-                Wishlist
+                <div className="flex items-center gap-2 text-white pt-[15px] p-2 border-b border-primary transition duration-300 hover:text-primary">
+                  <CgMail className="text-lg" /> mega@gmail.com
+                </div>
               </Link>
             </div>
           </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[16px] text-white">Helps</h1>
-            <div className="flex flex-col gap-3 text-gray-400 ">
-              <Link onClick={scrollToTop} to="/contact" className="hover:text-white text-[14px]">
-                Contact
-              </Link>
-              <Link onClick={scrollToTop} to="/contact" className="hover:text-white text-[14px]">
-                Faqs
-              </Link>
-              <Link onClick={scrollToTop} to="/contact" className="hover:text-white text-[14px]">
-                Terms & Condition
-              </Link>
-              <Link onClick={scrollToTop} to="/contact" className="hover:text-white text-[14px]">
-                Privacy Policy
-              </Link>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-[18px] text-white font-semibold">My Account</h1>
+            <div className="flex flex-col gap-2">
+              {['My Account', 'Order History', 'Shopping Cart', 'Wishlist'].map((item) => (
+                <Link key={item} onClick={scrollToTop} to="/" className="hover:text-primary transition duration-300">
+                  {item}
+                </Link>
+              ))}
             </div>
           </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[16px] text-white">Proxy</h1>
-            <div className="flex flex-col gap-3 text-gray-400 ">
-              <Link onClick={scrollToTop} to="/card" className="hover:text-white text-[14px]">
-                About
-              </Link>
-              <Link onClick={scrollToTop} to="/card" className="hover:text-white text-[14px]">
-                Shop
-              </Link>
-              <Link onClick={scrollToTop} to="/card" className="hover:text-white text-[14px]">
-                Product
-              </Link>
-              <Link onClick={scrollToTop} to="/card" className="hover:text-white text-[14px]">
-                Track Order
-              </Link>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-[18px] text-white font-semibold">Help</h1>
+            <div className="flex flex-col gap-2">
+              {['Contact', 'FAQs', 'Terms & Condition', 'Privacy Policy'].map((item) => (
+                <Link key={item} onClick={scrollToTop} to="/contact" className="hover:text-primary transition duration-300">
+                  {item}
+                </Link>
+              ))}
             </div>
           </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[16px] text-white">Categories</h1>
-            <div className="flex flex-col gap-3 text-gray-400 ">
-              <Link onClick={scrollToTop} to="/drinkIndex" className="hover:text-white text-[14px]">
-                Drink & Watert
-              </Link>
-              <Link onClick={scrollToTop} to="/yogurt" className="hover:text-white text-[14px]">
-                Yogurt & Ice Cream
-              </Link>
-              <Link onClick={scrollToTop} to="/cake" className="hover:text-white text-[14px]">
-                Cake & Bread
-              </Link>
-              <Link onClick={scrollToTop} to="/butter" className="hover:text-white text-[14px]">
-                Butter & Cream
-              </Link>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-[18px] text-white font-semibold">Proxy</h1>
+            <div className="flex flex-col gap-2">
+              {['About', 'Shop', 'Product', 'Track Order'].map((item) => (
+                <Link key={item} onClick={scrollToTop} to="/card" className="hover:text-primary transition duration-300">
+                  {item}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-[18px] text-white font-semibold">Categories</h1>
+            <div className="flex flex-col gap-2">
+              {['Drink & Water', 'Yogurt & Ice Cream', 'Cake & Bread', 'Butter & Cream'].map((item) => (
+                <Link key={item} onClick={scrollToTop} to={`/${item.replace(/ & /g, '').replace(/ /g, '').toLowerCase()}`} className="hover:text-primary transition duration-300">
+                  {item}
+                </Link>
+              ))}
             </div>
           </div>
         </div>
 
-        <div className="w-full h-[1px] bg-gray-200"></div>
-        <div className="py-6 flex items-center justify-between">
-          <div className="">
-            <a href="#1" className="text-gray-500 hover:text-white">
-              MegaCenter eCommerce © 2024. All Rights Reserved
-            </a>
-          </div>
-          <div className="text-white flex items-center gap-2">
-            <div className="border rounded-lg border-r-gray-500 py-1 inline-block px-3 cursor-pointer">
-              <FaCcApplePay size={26} />
-            </div>{' '}
-            <div className="border rounded-lg border-r-gray-500 py-1 inline-block px-3 cursor-pointer">
-              <FaCcVisa size={26} />
-            </div>{' '}
-            <div className="border rounded-lg border-r-gray-500 py-1 inline-block px-3 cursor-pointer">
-              <FaCcDiscover size={26} />
-            </div>{' '}
-            <div className="border rounded-lg border-r-gray-500 py-1 inline-block px-3 cursor-pointer">
-              <FaCcMastercard size={26} />
-            </div>{' '}
-            <div className="border border-gray-500 rounded-lg flex items-center py-1 px-4 justify-center h-[36px]">
-              <div className="flex flex-col cursor-pointer">
-                <p className="flex items-center gap-1 text-[10px]">
-                  <RiSecurePaymentFill size={12} />
-                  Secure
-                </p>
-                <p className="text-[12px]">Payment</p>
+        <div className="border-t border-gray-700 mt-8"></div>
+
+        <div className="flex flex-wrap justify-between items-center pt-3">
+          <p className="text-xs text-center w-full lg:w-auto">&copy; 2024 MegaCenter eCommerce. All Rights Reserved.</p>
+          <div className="flex items-center gap-4 mt-4 lg:mt-0">
+            {[FaCcApplePay, FaCcVisa, FaCcDiscover, FaCcMastercard].map((Icon, index) => (
+              <Icon key={index} className="text-2xl cursor-pointer hover:text-primary transition duration-300 transform hover:scale-110" />
+            ))}
+            <div className="flex items-center border border-gray-700 rounded-lg p-2 cursor-pointer hover:border-primary transition duration-300">
+              <RiSecurePaymentFill className="text-lg" />
+              <div className="ml-2 text-center">
+                <p className="text-xs">Secure</p>
+                <p className="text-xs">Payment</p>
               </div>
             </div>
+          </div>
+          <div className="flex gap-4 mt-4 lg:mt-0">
+            {[FaFacebook, FaTwitter, FaInstagram, FaLinkedin].map((Icon, index) => (
+              <Icon key={index} className="text-xl text-white cursor-pointer hover:text-primary transition duration-300 transform hover:scale-110" />
+            ))}
           </div>
         </div>
       </div>
